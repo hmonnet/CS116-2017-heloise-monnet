291b51dff57e54a5ecf46b4b7460bb2cc0106b65
hmonnet <32634645+hmonnet@users.noreply.github.com>
2017-11-11 17:06:53 +0100

    Delete CTestTestfile.cmake

--- src/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/INTRANET/hmonnet/myfiles/BA3/CS116-2017-heloise-monnet/src
-# Build directory: /home/INTRANET/hmonnet/myfiles/BA3/CS116-2017-heloise-monnet/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_test "unit_test")
-subdirs(gtest)
